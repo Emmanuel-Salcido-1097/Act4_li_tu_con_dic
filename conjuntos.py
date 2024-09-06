1a89cdeb3b85a59d40665838fc731d536624ef30
Emmanuel-Salcido-1097 <164534125+Emmanuel-Salcido-1097@users.noreply.github.com>
2024-09-06 12:13:21 -0600

    Add files via upload

---
+++ conjuntos.py
@@ -0,0 +1,6 @@
+miscolores = {"rojo", "amarillo", "verde"}
+print(miscolores)
+miscolores1 = {"rojo", "amarillo", "verde"}
+print(len(miscolores1))
+for x in miscolores1:
+    print(x)
---
+++ diccionario.py
@@ -0,0 +1,15 @@
+infoalumno = {
+    "Nombre": "Carlos",
+    "Apellido": "Melgar",
+    "Edad": 17
+}
+print(infoalumno)
+infoalumno1 = {
+    "Nombre": "David",
+    "Apellido": "Salazar",
+    "Edad": 17
+}
+print(infoalumno1["Nombre"])
+print("--Loop--")
+for x in infoalumno1:
+    print(infoalumno1[x])
